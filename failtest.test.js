bb77e14df6466dbca0cfb8e6a5c0fb7d6e618b69
L.P. Janssens <l.p.janssens@umail.leidenuniv.nl>
2022-10-03 12:05:02 +0200

    Deliberate fail test file

--- failtest.test.js
+++ failtest.test.js
@@ -9,7 +9,13 @@ test(
   const TOTAL = addition(1,2);
   
   // Assert
-  expect(TOTAL).toBe(5);
+  // Test yourself what happens when you change the 3 to something else
+  // You should notice already that the debugger tells you it is wrong
+  // Furthermore, after pushing, Github will send you a notification
+  // not all tests have passed. There will be no checkmark, but instead a 
+  // red cross. If you open the test environment, you will notice
+  // that app.test.js has passed all tests, but not this file.
+  expect(TOTAL).toBe(3);
   }
 );
 // Test must fail. 
